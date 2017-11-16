5542e7c07140c3cd7cd9ce89d935c04d475748d5
amarcinkowski <andrzej.marcinkowski@gmail.com>
2017-11-17 00:42:52 +0100

    add tdd tests

--- src/test/groovy/io/github/amarcinkowski/BoardSpec.groovy
+++ src/test/groovy/io/github/amarcinkowski/BoardSpec.groovy
@@ -10,16 +10,16 @@ class BoardSpec extends Specification {
     def "Board should look like this"() {
         given:
         def board = new Board()
-        expect:
-        println "xxxx${board}XXXX"
-        assert board ==
-                "_ _ _ _ _ _ _ _\n" +
-                "_ _ _ _ _ _ _ _\n" +
-                "_ _ _ _ _ _ _ _\n" +
-                "_ _ _ _ _ _ _ _\n" +
-                "p p p p p p p p\n" +
+        def expected =
                 "r n b q k b n r\n" +
-                "P P P P P P P P\n" +
-                "R N B Q K B N R"
+                        "p p p p p p p p\n" +
+                        "_ _ _ _ _ _ _ _\n" +
+                        "_ _ _ _ _ _ _ _\n" +
+                        "_ _ _ _ _ _ _ _\n" +
+                        "_ _ _ _ _ _ _ _\n" +
+                        "P P P P P P P P\n" +
+                        "R N B Q K B N R\n"
+        expect:
+        assert board.toString() == expected
     }
 }
