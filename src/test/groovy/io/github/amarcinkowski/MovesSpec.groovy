97938657132982a3ccfb5fdaa32161ead1a89de6
amarcinkowski <andrzej.marcinkowski@gmail.com>
2017-11-26 02:35:02 +0100

    added test cases for black

--- src/test/groovy/io/github/amarcinkowski/MovesSpec.groovy
+++ src/test/groovy/io/github/amarcinkowski/MovesSpec.groovy
@@ -17,6 +17,7 @@ class MovesSpec extends Specification {
         assert new Move(board: board, from: Square.valueOf(from), to: Square.valueOf(to)).isValid() == valid
         where:
         from | to   | valid
+        // white
         'B2' | 'B3' | true
         'C2' | 'C3' | true
         'C2' | 'C4' | true
@@ -26,6 +27,16 @@ class MovesSpec extends Specification {
         'A3' | 'A5' | false
         'B1' | 'C1' | false
         'B1' | 'D3' | false
+        'B2' | 'D5' | false
+        // black
+        'B7' | 'B6' | true
+        'C7' | 'C6' | true
+        'C7' | 'C5' | true
+        'B7' | 'B5' | true
+        'B7' | 'B8' | false
+        'A8' | 'A6' | false
+        'B7' | 'C7' | false
+        'B7' | 'D7' | false
 
     }
 }
